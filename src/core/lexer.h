68ab03e5b470feffa69b7f99c228051b209c5b0a
ramez-hammad <hammad.ramez001@gmail.com>
2025-06-22 14:17:55 +0300

    Update lexer.h

--- src/core/lexer.h
+++ src/core/lexer.h
@@ -7,12 +7,25 @@ typedef enum TOKEN_TYPE {
     TOKEN_NUM,
     TOKEN_LEFT_PAREN,
     TOKEN_RIGHT_PAREN,
-    TOKEN_EQ
+    TOKEN_EQ,
+    TOKEN_SIN,
+    TOKEN_COS,
+    TOKEN_TAN,
+    TOKEN_SINH,
+    TOKEN_COSH,
+    TOKEN_TANH,
+    TOKEN_ASIN,
+    TOKEN_ACOS,
+    TOKEN_ATAN,
+    TOKEN_ASINH,
+    TOKEN_ACOSH,
+    TOKEN_ATANH
 } TOKEN_TYPE;
 
 typedef struct TOKEN {
     TOKEN_TYPE type;
     double val; // Only used for numbers
+    double angle;  // Only used for trigonometric functions
 } TOKEN;
 
 TOKEN** tokenize(char* expr);
