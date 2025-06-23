15fdb75a94b0df644d6044f7f3cd2575fa15cb19
ramez-hammad <hammad.ramez001@gmail.com>
2025-06-23 15:14:36 +0300

    Update unit test for lexer

--- test/test_lexer.c
+++ test/test_lexer.c
@@ -13,8 +13,15 @@ void tearDown()
 void test_num_tokens()
 {
     int* num_tokens = (int*)malloc(sizeof(int));
-    TOKEN* output = tokenize("3X^2+23X^3+23+265+sin(234)-sinh(43)+log(2)", num_tokens);
+
+    tokenize("3X^2+23X^3+23+265+sin(234)-sinh(43)+log(2)", num_tokens);
     TEST_ASSERT_EQUAL(19, *num_tokens);
+    
+    tokenize("33000X^23234342+345^XXXX+243434+2XYZ^3234+ln(232.2)+log(2334)", num_tokens);
+    TEST_ASSERT_EQUAL(24, *num_tokens);
+
+    tokenize("acosh(234)+32000*sin(32)+2334sin(23)", num_tokens);
+    TEST_ASSERT_EQUAL(8, *num_tokens);
 }
 
 int main(void)
