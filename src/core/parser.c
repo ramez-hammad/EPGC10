676e396ad1a840f26d2c517828361132eb031690
ramez-hammad <hammad.ramez001@gmail.com>
2025-07-02 17:22:33 +0300

    Add parsing of parentheses

--- src/core/parser.c
+++ src/core/parser.c
@@ -6,9 +6,11 @@
 
 int next_index = 0;
 
-char *expr = "(3+3)+3*3";
+char *expr = "(3+4*3)+(3+3*4)";
 
 int num_tokens;
+
+int num_paren = 0;
 
 NODE *create_node_lit(double val)
 {
@@ -36,28 +38,43 @@ TOKEN next_token(char look_ahead)
         if (next_index < num_tokens) {
             next = arr_tok[next_index];
             next_index++;
+            return next;
         } else {
             return create_token_op(TOKEN_NULL);
         }
     } else {
         if (next_index < num_tokens) {
             return arr_tok[next_index];
+        } else {
+            return create_token_op(TOKEN_NULL);
         }
-        return create_token_op(TOKEN_NULL);
     }
-    return next;
 }
 
 // factor -> number
 //        -> (expression)
 NODE *parse_factor(TOKEN *current_token)
 {
-    NODE *current_factor;
+    NODE *current_factor = (NODE *) malloc(sizeof(NODE));
     switch (current_token->type) {
         case TOKEN_NUM:
             current_factor = create_node_lit(current_token->val);
+            if (next_token(1).type == TOKEN_RIGHT_PAREN) goto right_paren;
+            *current_token = next_token(0);
+            return current_factor;
+        case TOKEN_LEFT_PAREN:
+            num_paren++;
+            if (next_token(1).type == TOKEN_RIGHT_PAREN); // Syntax Error
+            current_factor = parse_expression();
+            *current_token = next_token(0);
             return current_factor;
     }
+
+right_paren:
+    num_paren--;
+    next_token(0);
+    *current_token = create_token_op(TOKEN_NULL);
+    return current_factor;
 }
 
 // term -> factor *|/ factor
@@ -70,17 +87,14 @@ NODE *parse_term(TOKEN *current_token)
         if (current_token->type == TOKEN_MUL) {
             *current_token = next_token(0);
             current_term = create_node_op(TOKEN_MUL, current_term, parse_factor(current_token));
-            *current_token = next_token(0);
         } else if (current_token->type == TOKEN_DIV) {
             *current_token = next_token(0);
             current_term = create_node_op(TOKEN_DIV, current_term, parse_factor(current_token));
-            *current_token = next_token(0);
         } else if (current_token->type == TOKEN_MINUS || current_token->type == TOKEN_PLUS || current_token->type ==
                    TOKEN_NULL) {
             return current_term;
         } else {
             current_term = parse_factor(current_token);
-            *current_token = next_token(0);
         }
     }
 }
@@ -102,16 +116,24 @@ NODE *parse_expression()
         } else if (current_token.type == TOKEN_MINUS) {
             current_token = next_token(0);
             current_node = create_node_op(TOKEN_MINUS, current_node, parse_term(&current_token));
+        } else if (current_token.type == TOKEN_NULL) {
+            return current_node;
         } else {
             current_node = parse_term(&current_token);
         }
     }
+
+    if (num_paren != 0) {
+        // Syntax error (Mismatched parentheses)
+    }
+
     return current_node;
 }
 
 int main(void)
 {
     NODE *root = parse_expression();
+    //printf("%d", num_paren);
     free(root);
     return 0;
 }
